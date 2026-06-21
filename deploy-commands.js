a820eee95f06185cec69af148613b01aa11ef5b9
UnpopularFluffy <hobibihobibi57@gmail.com>
2026-06-21 21:11:54 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,60 @@
+const {
+    REST,
+    Routes
+} = require("discord.js");
 
+const fs = require("fs");
+const path = require("path");
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const commands = [];
+
+const commandsPath = path.join(__dirname, "commands");
+
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+
+    const command = require(path.join(commandsPath, file));
+
+    commands.push(command.data.toJSON());
+
+}
+
+const rest = new REST({
+    version: "10"
+}).setToken(TOKEN);
+
+(async () => {
+
+    try {
+
+        console.log("Registering slash commands...");
+
+        await rest.put(
+
+            Routes.applicationGuildCommands(
+                CLIENT_ID,
+                GUILD_ID
+            ),
+
+            {
+                body: commands
+            }
+
+        );
+
+        console.log("Slash commands registered successfully.");
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+
+})();
